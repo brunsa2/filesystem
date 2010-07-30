2998eefd6b0367071eb83448dc43c0c880a8cfb2
Jeff Stubler <brunsa2@gmail.com>
2010-07-29 20:29:40 -0500

    folder returns list of files

--- Folder.php
+++ Folder.php
@@ -1,7 +1,24 @@
 <?php
 
 class Folder extends Tag {
+	public function __construct($id) {
+		parent::__construct($id);
+		
+		$this->database->prepareQuery('SELECT id FROM links, assigns WHERE links.id = assigns.link AND assigns.tag = ' . $this->database->name('id'))->storeQuery('Folder-GetFiles');
+	}
 	
+	public function getFilesInFolder() {
+		$this->database->retrieveQuery('Folder-GetFiles')->bindInteger('id', $this->id)->executeQuery();
+		
+		$files = array();
+		$filesPointer = 0;
+		
+		foreach($this->database as $row) {
+			$files[$filesPointer++] = new File($row->id, true, true, false);
+		}
+		
+		return $files;
+	}
 }
 
 ?>
