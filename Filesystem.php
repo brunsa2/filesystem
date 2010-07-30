6a6780423e7dc1a9b3d5915321f5a8d32277348d
Jeff Stubler <brunsa2@gmail.com>
2010-07-29 21:12:36 -0500

    filesystem stores pointers to top tag in hierarchy and the root folder tag

--- Filesystem.php
+++ Filesystem.php
@@ -8,12 +8,23 @@ class Filesystem {
 	
 	private static $instance;
 	
+	private static $rootTag = new Tag(1);
+	private static $rootFolder = new Folder(2);
+	
 	public function getFilesystem() {
 		if(self::$instance == null) {
 			self::$instance = new Filesystem();
 		}
 		
 		return self::$instance;
+	}
+	
+	public static function getRootTag() {
+		return self::$rootTag;
+	}
+	
+	public static function getRootFolder() {
+		return self::$rootFolder;
 	}
 	
 	private function __construct() {
