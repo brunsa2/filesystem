ff10ec2ec7ac36f34c4837d87b99069f5eaf08f3
Jeff Stubler <brunsa2@gmail.com>
2010-07-29 21:08:17 -0500

    added support for filesystem exceptions starting with an invalid folder

---
+++ FilesystemException.php
@@ -0,0 +1,30 @@
+<?php
+
+class FilesystemException extends Exception {
+	const FOLDER_DOES_NOT_EXIST_EXCEPTION = 0x01;
+	const INVALID_EXCEPTION = 0xff;
+	
+	private $exceptionMessages = array(
+		self::FOLDER_DOES_NOT_EXIST_EXCEPTION => 'Folder does not exist',
+		self::INVALID_EXCEPTION => 'Invalid exception'
+	);
+	
+	private $code;
+	private $extraData;
+	
+	public function __construct($code, $extraData = null) {
+		if(is_int($code) && $code >= 0x00 && $code < 0x100) {
+			$this->code = $code;
+			$this->message = $this->exceptionMessages[$code];
+			$this->extraData = $extraData;
+		} else {
+			throw new FilesystemException(self::INVALID_EXCEPTION, $code)
+		}
+	}
+	
+	public function getExtraData() {
+		return $this->extraData;
+	}
+}
+
+?>
