4922b01e4bdd46cdac0960f253ce6fc42fdd3cf2
Jeff Stubler <brunsa2@gmail.com>
2010-07-30 17:10:29 -0500

    trying to move database query declarations out because of issue where new queries are stored and overwriting old ones

--- Folder.php
+++ Folder.php
@@ -10,18 +10,11 @@ class Folder {
 		$this->id = $id;
 		
 		$this->database = Database::getDatabase();
-		$this->database->prepareQuery('SELECT name FROM folders WHERE id = ' . $this->database->name('id'))->storeQuery('Folder-GetFolder');
 		$this->database->bindInteger('id', $id)->executeQuery();
 		
 		foreach($this->database as $row) {
 			$this->name = $row->name;
 		}
-		
-		$this->database->prepareQuery('UPDATE folders SET name = ' . $this->database->name('name') . ' WHERE id = ' . $this->database->name('id'))->storeQuery('Folder-SetFolderName');
-		$this->database->prepareQuery('SELECT id FROM links WHERE folder = ' . $this->database->name('id'))->storeQuery('Folder-GetFiles');
-		$this->database->prepareQuery('SELECT id FROM folders WHERE parentfolder = ' . $this->database->name('id'))->storeQuery('Folder-GetFolders');
-		$this->database->prepareQuery('INSERT INTO folders VALUES(NULL, :id, :name)')->storeQuery('Folder-MakeFolder');
-		$this->database->prepareQuery('SELECT id FROM folders WHERE parentfolder = ' . $this->database->name('id') . ' AND name = ' . $this->database->name('name'))->storeQuery('Folder-GetNewFolder');
 	}
 	
 	public function name($name = '') {
@@ -49,11 +42,16 @@ class Folder {
 	public function getFoldersInFolder() {
 		$this->database->retrieveQuery('Folder-GetFolders')->bindInteger('id', $this->id)->executeQuery();
 		
+		echo $this->database . '<br />';
+		echo $this->id . '<br />';
+		echo $this->database->getNumberOfRows() . '<br />';
+		
 		$folders = array();
 		$foldersPointer = 0;
 		
 		foreach($this->database as $row) {
-			$folders[$foldersPointer++] = new Folder($row->id);
+			print_r($row);
+			$folders[$foldersPointer++] = new Folder(1);
 		}
 		
 		return $folders;
