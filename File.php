63110be26537ae67b494f29b3bbbd0f7f4876973
Jeff Stubler <brunsa2@gmail.com>
2010-07-31 19:10:11 -0500

    switched file class to new database access

--- File.php
+++ File.php
@@ -22,19 +22,19 @@ class File {
 		$this->id = $id;
 
 		$this->database = Database::getDatabase();
-		$this->database->prepareQuery('SELECT hash, content FROM links, files WHERE links.id = ' . $this->database->name('id') . ' AND links.file = files.id')->storeQuery('File-GetFile');
-		$this->database->bindInteger('id', $id)->executeQuery();
+		$this->database->prepareQuery('SELECT hash, content FROM links, files WHERE links.id = ' . name('id') . ' AND links.file = files.id', 'File-GetFile');
+		$this->database->bindInteger('id', $id);
 		
-		foreach($this->database as $row) {
+		foreach($this->database->executeQuery() as $row) {
 			$this->content = $row->content;
 			$this->size = strlen($row->content);
 			$this->hash = $row->hash;
 		}
 		
-		$this->database->prepareQuery('SELECT name FROM links WHERE id = ' . $this->database->name('id'))->storeQuery('File-GetName');
-		$this->database->bindInteger('id', $id)->executeQuery();
+		$this->database->prepareQuery('SELECT name FROM links WHERE id = ' . name('id'), 'File-GetName');
+		$this->database->bindInteger('id', $id);
 		
-		foreach($this->database as $row) {
+		foreach($this->database->executeQuery() as $row) {
 			$this->name = $row->name;
 		}
 		
@@ -44,9 +44,9 @@ class File {
 		$this->size = strlen($this->content);
 		$this->position = 0;
 		
-		$this->database->prepareQuery('SELECT hash, content FROM links, files WHERE links.id = ' . $this->database->name('id') . ' AND links.file = files.id AND hash != ' . $this->database->name('hash'))->storeQuery('File-GetChangedFile');
-		$this->database->prepareQuery('UPDATE links, files SET files.content = ' . $this->database->name('content') . ', files.hash = SHA1(' . $this->database->name('content') . ') WHERE links.id = ' . $this->database->name('id') . ' AND links.file = files.id')->storeQuery('File-StoreFile');
-		$this->database->prepareQuery('UPDATE links SET name = ' . $this->database->name('name') . ' WHERE id = ' . $this->database->name('id'))->storeQuery('File-Rename');
+		$this->database->prepareQuery('SELECT hash, content FROM links, files WHERE links.id = ' . name('id') . ' AND links.file = files.id AND hash != ' . name('hash'), 'File-GetChangedFile');
+		$this->database->prepareQuery('UPDATE links, files SET files.content = ' . name('content') . ', files.hash = SHA1(' . name('content') . ') WHERE links.id = ' . name('id') . ' AND links.file = files.id', 'File-StoreFile');
+		$this->database->prepareQuery('UPDATE links SET name = ' . name('name') . ' WHERE id = ' . name('id'), 'File-Rename');
 	}
 	
 	public function close() {
@@ -66,12 +66,7 @@ class File {
 	}
 	
 	public function peek($length = 0) {
-		$this->database->retrieveQuery('File-GetChangedFile')->bindInteger('id', $this->id)->bindInteger('hash', $this->hash)->executeQuery();
-		foreach($this->database as $row) {
-			$this->content = $row->content;
-			$this->size = strlen($row->content);
-			$this->hash = $row->hash;
-		}
+		$this->updateFileContents();
 		
 		if($this->readEnable) {
 			$length = $length == 0 ? $this->size - $this->position : $length;
@@ -84,12 +79,7 @@ class File {
 	
 	public function truncate($length) {
 		if($this->writeEnable) {
-			$this->database->retrieveQuery('File-GetChangedFile')->bindInteger('id', $this->id)->bindInteger('hash', $this->hash)->executeQuery();
-			foreach($this->database as $row) {
-				$this->content = $row->content;
-				$this->size = strlen($row->content);
-				$this->hash = $row->hash;
-			}
+			$this->updateFileContents();
 			
 			if($length > $this->size) {
 				for($currentNullCharacter = 0; $currentNullCharacter < ($length - $this->size); $currentNullCharacter++) {
@@ -101,7 +91,7 @@ class File {
 			
 			$this->size = $length;
 			
-			$this->database->retrieveQuery('File-StoreFile')->bindInteger('id', $this->id)->bindInteger('content', $this->content)->executeQuery();
+			$this->database->select('File-StoreFile')->bindInteger('id', $this->id)->bindInteger('content', $this->content)->executeQuery();
 			
 			return true;
 		} else {
@@ -111,13 +101,8 @@ class File {
 	
 	public function seek($offset, $relative = self::SEEK_CURRENT) {
 		if($this->readEnable || ($this->writeEnable && !$this->appendOnly)) {
-			$this->database->retrieveQuery('File-GetChangedFile')->bindInteger('id', $this->id)->bindInteger('hash', $this->hash)->executeQuery();
-			foreach($this->database as $row) {
-				$this->content = $row->content;
-				$this->size = strlen($row->content);
-				$this->hash = $row->hash;
-			}
-		
+			$this->updateFileContents();
+			
 			$position = $this->position + $offset;
 			
 			switch($relative) {
@@ -142,12 +127,7 @@ class File {
 	
 	public function write($data, $length = 0) {
 		if($this->writeEnable) {
-			$this->database->retrieveQuery('File-GetChangedFile')->bindInteger('id', $this->id)->bindInteger('hash', $this->hash)->executeQuery();
-			foreach($this->database as $row) {
-				$this->content = $row->content;
-				$this->size = strlen($row->content);
-				$this->hash = $row->hash;
-			}
+			$this->updateFileContents();
 			
 			$length = $length == 0 ? strlen($data) : $length;
 			$position = $this->appendOnly ? $this->size : $this->position;
@@ -159,7 +139,7 @@ class File {
 			
 			$this->content = substr($this->content, 0, $position) . $data . substr($this->content, $position + $length);
 			
-			$this->database->retrieveQuery('File-StoreFile')->bindInteger('id', $this->id)->bindInteger('content', $this->content)->executeQuery();
+			$this->database->select('File-StoreFile')->bindInteger('id', $this->id)->bindInteger('content', $this->content)->executeQuery();
 			
 			return $length;
 		} else {
@@ -169,11 +149,21 @@ class File {
 	
 	public function name($name = '') {
 		if($name != null & $name != '') {
-			$this->database->retrieveQuery('File-Rename')->bindString('name', $name)->bindInteger('id', $this->id)->executeQuery();
+			$this->database->select('File-Rename')->bindString('name', $name)->bindInteger('id', $this->id)->executeQuery();
 			$this->name = $name;
 		}
 		
 		return $this->name;
+	}
+	
+	private function updateFileContents() {
+		$this->database->select('File-GetChangedFile')->bindInteger('id', $this->id)->bindString('hash', $this->hash);
+		
+		foreach($this->database->executeQuery() as $row) {
+			$this->content = $row->content;
+			$this->size = strlen($row->content);
+			$this->hash = $row->hash;
+		}
 	}
 }
 
